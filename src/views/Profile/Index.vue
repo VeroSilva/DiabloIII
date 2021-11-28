c0b5f0c0d3e7faa9581509f941717b3ec8d4403c
Verónica Silva <vsilva@grupoecce.com>
2021-11-28 10:00:50 -0500

    Fetching data and updating Profile

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,11 +1,59 @@
 <template>
   <div>
+    <BaseLoading v-if="isLoading" />
     <h1>Profile View</h1>
+    <template v-if="profileData !== null">
+      <MainBlock :profile-data="profileData"/>
+    </template>
   </div>
 </template>
 
 <script>
+import setError from '@/mixins/setError'
+import { getApiAccount } from '@/api/search'
+import BaseLoading from '@/components/BaseLoading'
+import MainBlock from './MainBlock/Index'
+
 export default {
-  name: 'ProfileView'
+  name: 'ProfileView',
+  components: { BaseLoading, MainBlock },
+  mixins: [
+    setError
+  ],
+  data () {
+    return {
+      profileData: null,
+      isLoading: false
+    }
+  },
+  created () {
+    this.isLoading = true
+    const { region, battleTag: account } = this.$route.params
+    this.fetchData(region, account)
+  },
+  methods: {
+    fetchData (region, account) {
+      getApiAccount({ region, account })
+        .then(({ data }) => {
+          this.profileData = data
+        })
+        .catch((err) => {
+          this.profileData = null
+          const errObj = {
+            routeParams: this.$route.params,
+            message: err.message
+          }
+          if (err.response) {
+            errObj.data = err.response.data
+            errObj.status = err.response.status
+          }
+
+          this.setApiErr(errObj)
+          this.$router.push({ name: 'Error' })
+        }).finally(() => {
+          this.isLoading = false
+        })
+    }
+  }
 }
 </script>
