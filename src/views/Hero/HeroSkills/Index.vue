3d8247fd4516a6a1a72693cce077f488d3d64473
Verónica Silva <vsilva@grupoecce.com>
2021-12-02 20:28:56 -0500

    Lazy load in active and passive skills

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -3,27 +3,56 @@
     <h2 class="font-diablo">Skills</h2>
     <hr class="bg-white">
 
+    <b-nav pills small>
+      <b-nav-item :active="!isPassiveSkillsActive" @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+      <b-nav-item :active="isPassiveSkillsActive" @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+    </b-nav>
+
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps"/>
+    </keep-alive>
+    <!--
     <ActiveSkills :skills="skills.active"/>
     <hr>
     <PassiveSkills :skills="skills.passive"/>
+    -->
 
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
-
 export default {
   name: 'HeroSkills',
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () => import(/* webpackChunkName: "ActiveSkills" */'./ActiveSkills'),
+    PassiveSkills: () => import(/* webpackChunkName: "PassiveSkills" */'./PassiveSkills')
   },
   props: {
     skills: {
       required: true,
       type: Object
+    }
+  },
+  computed: {
+    /**
+     * Dinamyc props for async dynamic components
+     * @returns {String}
+     */
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills' ? this.skills.active : this.skills.passive
+    },
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
+    }
+  },
+  methods: {
+    changeComponent (component) {
+      this.activeComponent = component
     }
   }
 }
